52010c82e359b3b0a34d5afb30f0f7db5a1e095c
Kasper Andkjaer <kan@invert.dk>
2010-11-13 12:27:01 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gemtester::Application.config.secret_token = '78da2d8cd7373a6269113bd6eaa56d1f0a8a8fc89c8536e6403e6a4469bc90f47b961b8cab6c4ae235638552c85cace608176bb359ee0ce96501ededf63d2d68'
